ad4dffef9dfecc3cba3fc24e9c3f2279452e210f
Polad Mamedov <poladikksp@gmail.com>
2023-02-17 15:21:52 +0200

    added closing the menu when resized

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,13 @@ menuBtn.addEventListener("click", (e) => {
   }
   menu.classList.toggle("main-header__menu--show");
 });
+window.addEventListener("resize", () => {
+  if (
+    window.matchMedia("(min-width: 768px)").matches &&
+    menu.classList.contains("main-header__menu--show")
+  ) {
+    menu.classList.remove("main-header__menu--show");
+    menuBtn.children[0].classList.remove("main-header__brgrbtnsvg--hidden");
+    menuBtn.children[1].classList.add("main-header__brgrbtnsvg--hidden");
+  }
+});
